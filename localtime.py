4bddb244857060f6e9725b71435a6e280d13726d
Caio <caio.a.santos@gmail.com>
2017-05-17 14:47:39 -0300

    Update localtime.py

--- localtime.py
+++ localtime.py
@@ -21,3 +21,15 @@ if DWT = "monday"
 else
     print("NoK")
 
+def alarm_clock(day, on_vacation):
+    '''Alarm Clock'''
+    if on_vacation:
+        if day in range(1,6):
+            return '10:00'
+        elif day in [0,6]:
+            return 'off'
+    else:
+        if day in range(1,6):
+            return '7:00'
+        elif day in [0,6]:
+            return '10:00'
